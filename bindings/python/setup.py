f702605ffae73b64081b3d48f9633225397aa617
Hamish Hall <hachall@hotmail.com>
2026-03-30 12:39:10 +0100

    fix: not releasing (#43)

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -8,4 +8,23 @@ class BinaryDistribution(Distribution):
         return True
 
 
-setup(distclass=BinaryDistribution)
+cmdclass = {}
+try:
+    from wheel.bdist_wheel import bdist_wheel
+
+    class BinaryWheel(bdist_wheel):
+        """Override wheel tags: preserve python tag, force ABI to none."""
+        def finalize_options(self):
+            super().finalize_options()
+            self.root_is_pure = False
+
+        def get_tag(self):
+            python, _abi, plat = super().get_tag()
+            return python, "none", plat
+
+    cmdclass["bdist_wheel"] = BinaryWheel
+except ImportError:
+    pass
+
+
+setup(distclass=BinaryDistribution, cmdclass=cmdclass)
