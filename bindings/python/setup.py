bae7d97503a8670f6821c1b6d381fa7b80fdbe06
hachall <hachall@hotmail.com>
2026-03-30 11:51:06 +0100

    import guard

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 from setuptools.dist import Distribution
-from wheel.bdist_wheel import bdist_wheel
 
 
 class BinaryDistribution(Distribution):
@@ -9,11 +8,19 @@ class BinaryDistribution(Distribution):
         return True
 
 
-class BinaryWheel(bdist_wheel):
-    """Override wheel tags to produce py3-none-<platform>."""
-    def get_tag(self):
-        _python, _abi, plat = super().get_tag()
-        return "py3", "none", plat
+cmdclass = {}
+try:
+    from wheel.bdist_wheel import bdist_wheel
+
+    class BinaryWheel(bdist_wheel):
+        """Override wheel tags to produce py3-none-<platform>."""
+        def get_tag(self):
+            _python, _abi, plat = super().get_tag()
+            return "py3", "none", plat
+
+    cmdclass["bdist_wheel"] = BinaryWheel
+except ImportError:
+    pass
 
 
-setup(distclass=BinaryDistribution, cmdclass={"bdist_wheel": BinaryWheel})
+setup(distclass=BinaryDistribution, cmdclass=cmdclass)
